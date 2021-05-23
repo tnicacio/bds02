64d9eb0a4a71b33181a25d3a80a44b521888d473
tnicacio <nicacio.t92@gmail.com>
2021-05-23 00:18:01 -0300

    City Controller DataIntegrityIT Tests passing

--- src/main/java/com/devsuperior/bds02/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/bds02/controllers/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -26,16 +27,16 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status.value()).body(err);
 	}
 
-//	@ExceptionHandler(DatabaseException.class)
-//	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
-//		HttpStatus status = HttpStatus.BAD_REQUEST;
-//		StandardError err = new StandardError();
-//		err.setTimestamp(Instant.now());
-//		err.setStatus(status.value());
-//		err.setError("Resource not found");
-//		err.setMessage(e.getMessage());
-//		err.setPath(request.getRequestURI());
-//		return ResponseEntity.status(status.value()).body(err);
-//	}
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status.value()).body(err);
+	}
 
 }
